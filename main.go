ae8561252d1e392f07effe5ef0046a531b7fb80a
Artur4ik488 <artur4ikoffical@gmail.com>
2026-08-29 07:42:32 +0300

    Add account deposit and withdrawal

--- main.go
+++ main.go
@@ -1,9 +1,57 @@
 package main
 
 import (
+	"errors"
 	"fmt"
 )
 
 func main() {
-	fmt.Println("BankApp is starting...")
+
+	account1 := Account{
+		ID:      1,
+		Owner:   "Arthur",
+		Balance: 300,
+	}
+
+	err := account1.Withdraw(400)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(account1.Balance)
+
+}
+
+type Account struct {
+	ID      int
+	Owner   string
+	Balance float64
+}
+
+func findAccountByID(accounts []Account, id int) (*Account, bool) {
+	for i, account := range accounts {
+		if account.ID == id {
+			return &accounts[i], true
+		}
+	}
+	return nil, false
+}
+
+func (account *Account) Deposit(amount float64) error {
+	if amount <= 0 {
+		return errors.New("deposit amount must be greater than zero")
+	}
+	account.Balance += amount
+	return nil
+}
+
+func (account *Account) Withdraw(amount float64) error {
+	if amount <= 0 {
+		return errors.New("amount must be greater than zero")
+	}
+	if amount > account.Balance {
+		return errors.New("insufficient funds")
+	}
+	account.Balance -= amount
+	return nil
 }
